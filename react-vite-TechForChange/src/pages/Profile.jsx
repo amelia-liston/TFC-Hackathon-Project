117e013d8c887456757a5a8b4d6c33d580f2a2d3
amelia-liston <aliston@bu.edu>
2024-02-17 18:45:27 -0500

    Working on filter in profile page

--- react-vite-TechForChange/src/pages/Profile.jsx
+++ react-vite-TechForChange/src/pages/Profile.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import items from "./allData";
+import { useState } from "react";
+import Menu from "./components/Menu";
+import Button from "./components/Button";
 
 export const Profile = () => {
+  const [menuItem, setMenuItem] = useState(items);
+  const [buttons, setButtons] = useState([]);
+
   return (
     <div>
       <h1>Hello, Bella!</h1>
@@ -11,7 +17,13 @@ export const Profile = () => {
         <img src="public/map.png"></img>
       </div>
       <h2 className="profile-h2">Sort by Category</h2>
-      <p></p>
+      <div className="title">
+        <h2>
+          Sort by Category <span>Filter</span>
+        </h2>
+      </div>
+      <Button />
+      <Menu menuItem={menuItem} />
     </div>
   );
 };
