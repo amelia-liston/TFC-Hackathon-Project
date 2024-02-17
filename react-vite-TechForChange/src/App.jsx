cdd334f7ab5ac1fad22a1a09caafb7966a0cdd1d
amelia-liston <aliston@bu.edu>
2024-02-17 16:36:28 -0500

    Still working on navbar

--- react-vite-TechForChange/src/App.jsx
+++ react-vite-TechForChange/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
-import { Navbar } from "./components/navbar";
+import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { Profile } from "./components/Profile";
+import { Feed } from "./components/Feed";
 
 function App() {
   return (
     <>
-      <body>
-        <div className="App">
-          <Navbar></Navbar>
-        </div>
-      </body>
+      <div className="App">
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- react-vite-TechForChange/src/components/navbar.jsx
+++ react-vite-TechForChange/src/components/navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Feed</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/feed">Feed</Link>
+        </li>
       </ul>
     </nav>
   );
