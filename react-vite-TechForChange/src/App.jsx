3b040c0dd4c1b748b53db9d32e8f6e500a3d1acd
amelia-liston <aliston@bu.edu>
2024-02-17 16:06:38 -0500

    restarting on trying to link pages by creating navbar componenet

--- react-vite-TechForChange/src/App.jsx
+++ react-vite-TechForChange/src/App.jsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <body>
-        <Router>
-          <navbar />
-          <Switch>
-            <Route path="/pages/Profile" exact component={Profile} />
-            <Route path="/pages/Feed" component={Feed} />
-            <Route path="/pages/More" component={More Info} />
-          </Switch>
-        </Router>
-        <h1>Get Civic</h1>
+        <div className="APP">Navbar</div>
       </body>
     </>
   );
--- react-vite-TechForChange/src/components/navbar.jsx
+++ react-vite-TechForChange/src/components/navbar.jsx
@@ -1,21 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-const navbar = () => {
+
+export const NavBar = () => {
   return (
     <div>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/cats">Profile</Link>
-      </li>
-      <li>
-        <Link to="/sheeps">Feed</Link>
-      </li>
-      <li>
-        <Link to="/goats">More</Link>
-      </li>
+      <Navbar></Navbar>
     </div>
   );
 };
-export default navbar;
