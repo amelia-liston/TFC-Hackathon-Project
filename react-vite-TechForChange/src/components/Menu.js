396c6b7defbefb20fd054aa84a847f4603b8389d
amelia-liston <aliston@bu.edu>
2024-02-17 19:10:33 -0500

    not working how i want it to, so going to make major changes

--- react-vite-TechForChange/src/components/Menu.js
+++ react-vite-TechForChange/src/components/Menu.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Menu() {
-  return <div></div>;
+function Menu({ menuItem }) {
+    return (
+        <div className="item">
+            {
+                menuItem.map((item) =>
+                    return 
+                        <div className="item=con" key={item.id}>
+                        <div className="item-container">
+                            <h2>{item.title}</h2>
+                            <p>{item.description}</p>
+                        </div>
+                    <div/>
+                )
+            }
+        </div>
+    )
 }
 
 export default Menu;
